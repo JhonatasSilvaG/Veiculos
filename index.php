1afacd4031418af17b4c284b4f05d41a88e12c1d
JhonatasSilvaG <jhonatas.sil.generoso@gmail.com>
2024-10-21 16:46:57 -0300

    Organização de pastas e estilização do projeto

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php require 'header.php' ?>
+<?php require 'partials/header.php' ?>
+<?php require 'partials/nav.php' ?>
+<main class="row">
     <form action="veiculos.php" method="post">
-        <label for="modelo">Modelo</label>
-        <input type="text" name="modelo">
-        <label for="ano">Ano</label>
-        <input type="text" name="ano">
-        <button>Enviar</button>
+        <div class="col-6 offset-3">
+            <div class="d-flex flex-column gap-3">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="modelo" name="modelo" placeholder="Modelo">
+                    <label for="floatingInput">Modelo</label>
+                </div>
+
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="ano" name="ano" placeholder="Ano">
+                    <label for="ano">Ano</label>
+                </div>
+
+                <button type="submmit" class="col-4 offset-4 btn btn-outline-primary">Cadastrar</button>
+            </div>
+        </div>
     </form>
+</main>
 
-<a class="btn btn-primary" href="veiculos.php">Ver tabela</a>
-
-<?php require 'rodape.php' ?>
+<?php require 'partials/rodape.php' ?>
--- veiculos.php
+++ veiculos.php
@@ -1,29 +1,29 @@
-<?php require 'header.php' ?>
+<?php require 'partials/header.php' ?>
+<?php require 'partials/nav.php' ?>
 
 <?php
-
-    $veiculos = [];
-
-    require 'function.php';
+    require 'functions.php';
 
     if (!empty($_POST['modelo']) && !empty($_POST['ano'])) {
-        escrita();
+        insert('data');
     }
 
-    $veiculos = mostrar();
+    $veiculos = select('data');
 ?>
 
-<table class="table">
-    <thead>
-        <th scope="col">Modelo</th>
-        <th scope="col">Ano</th>
-    </thead>
-    <?php foreach($veiculos as $veiculo) : ?>
-        <tr>
-            <td><?= $veiculo[0] ?></td>
-            <td><?= $veiculo[1] ?></td>
-        </tr>
-    <?php endforeach; ?>
-</table>
+<main class="offset-3 col-6 my-3" style="max-height: calc(100vh - 40px - 3rem); overflow-y: auto;">
+    <table class="table table-striped table-hover">
+        <thead>
+            <th scope="col">Modelo</th>
+            <th scope="col">Ano</th>
+        </thead>
+        <?php foreach($veiculos as $veiculo) : ?>
+            <tr>
+                <td><?= $veiculo[0] ?></td>
+                <td><?= $veiculo[1] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</main>
 
-<?php require 'rodape.php' ?>
+<?php require 'partials/rodape.php' ?>
